9ee37884744e3ad716a7279cad7b20dddf0d19bf
your name here <jennhuynh02@gmail.com>
2020-06-07 22:44:18 -0700

    Memoizing coin problems

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -1,3 +1,19 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amount, coins, memo = {}) {
+  if (amount === 0) return 1;
+  if (coins.length === 0) return 0;
+
+  let key = `${amount}-${coins.join(',')}`;
+  if (key in memo) return key[memo];
+
+  let coinVal = coins[coins.length - 1];
+
+  let total = 0;
+  for (let quantity = 0; quantity <= (amount/coinVal); quantity ++) {
+    total += change(amount - (quantity * coinVal, coins.slice(0, -1)), memo);
+  }
+  memo[key] = total;
+  return total;
+}
--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -39,7 +39,18 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
-    
+    if (amount === 0) return 0;
+    if (amount in memo) return memo[amount];
+
+    let change = [];
+    coins.forEach(coin => {
+        if (coin <= amount) {
+            change.push(minChange(coins, amount - coin, memo) + 1)
+        }
+    })
+
+    memo[amount] = Math.min(...change);
+    return memo[amount];
 }
 
 
