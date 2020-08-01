b1337254972da363abd7769bb09f6809f3ab8a8e
jennhuynh02 <jennhuynh02@gmail.com>
2020-08-01 00:22:09 -0700

    Analyzing Big O Time and Space Complexity

--- leet_code/javascript/validWordSquare.js
+++ leet_code/javascript/validWordSquare.js
@@ -1,15 +1,15 @@
 function validWordSquare(words) {
-  for (let i = 0; i < words.length; i++) {
-      for (let j = 0; j < words[i].length; j++) {
-        if (words[j]) {
-          // checks if there is a row that exists for that column's index e.g. if we're at the 2nd index of a word, but there is no 2nd indexed row exiting in square input, this would indicate that there is an insufficient match of lengths rendering the input invalid as in invalid square
-          if (words[i][j] !== words[j][i]) return false;
+  for (let i = 0; i < words.length; i++) { //O(n) number of strings
+      for (let j = 0; j < words[i].length; j++) { // O(m) longest length of substrings
+        if (words[j]) { // O(1)
+          // checks if there is a row that exists for that column's index e.g. if we're at the 2nd index of a word, but there is no 2nd indexed row exiting in square input, this would indicate that there is an insufficient match of lengths rendering the input as in invalid square
+          if (words[i][j] !== words[j][i]) return false; // O(1)
         } else {
-          return false;
+          return false; // O(1)
         }
       }
   } 
-  return true;
+  return true; // O(1)
 }
 
 let words1 = ["abcd", "bnrt", "crmy", "dtye"]
@@ -18,5 +18,5 @@ let words2 = ["abc", "b"]
 console.log(validWordSquare(words1))
 console.log(validWordSquare(words2))
 
-// Time Complexity: O(n^2)
+// Time Complexity: O(n * m)
 // Space Complexity: O(1)
