4e43e5618d6340733f97835dfa0a57113f7124f6
jennhuynh02 <jennhuynh02@gmail.com>
2020-08-01 00:54:54 -0700

    Shortening valid word square solution in js

--- leet_code/javascript/validWordSquare.js
+++ leet_code/javascript/validWordSquare.js
@@ -1,4 +1,4 @@
-function validWordSquare(words) {
+function validWordSquare1(words) {
   for (let i = 0; i < words.length; i++) { //O(n) number of strings
       for (let j = 0; j < words[i].length; j++) { // O(m) longest length of substrings
         if (words[j]) { // O(1)
@@ -12,11 +12,24 @@ function validWordSquare(words) {
   return true; // O(1)
 }
 
+// Attempt #2, shorter solution with modified conditional statement
+function validWordSquare2(words) {
+  for (let i = 0; i < words.length; i++) { //O(n) number of strings
+      for (let j = 0; j < words[i].length; j++) { // O(m) longest length of substrings
+        if ( j >= words.length || words[i][j] !== words[j][i]) return false; // O(1)
+      }
+  } 
+  return true; // O(1)
+}
+
 let words1 = ["abcd", "bnrt", "crmy", "dtye"]
 let words2 = ["abc", "b"]
 
-console.log(validWordSquare(words1))
-console.log(validWordSquare(words2))
+console.log(validWordSquare1(words1))
+console.log(validWordSquare1(words2))
+
+console.log(validWordSquare2(words1))
+console.log(validWordSquare2(words2))
 
 // Time Complexity: O(n * m)
 // Space Complexity: O(1)
