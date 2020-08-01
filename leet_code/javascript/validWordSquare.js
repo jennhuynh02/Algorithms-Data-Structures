3e613cdb50dcebdae36b18690d4d3696e0216851
jennhuynh02 <jennhuynh02@gmail.com>
2020-08-01 00:03:23 -0700

    Adding explanation comments

--- leet_code/javascript/validWordSquare.js
+++ leet_code/javascript/validWordSquare.js
@@ -2,6 +2,7 @@ function validWordSquare(words) {
   for (let i = 0; i < words.length; i++) {
       for (let j = 0; j < words[i].length; j++) {
         if (words[j]) {
+          // checks if there is a row that exists for that column's index e.g. if we're at the 2nd index of a word, but there is no 2nd indexed row exiting in square input, this would indicate that there is an insufficient match of lengths rendering the input invalid as in invalid square
           if (words[i][j] !== words[j][i]) return false;
         } else {
           return false;
