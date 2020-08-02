a3dfd20016bf1af02db7d890166f9183dbb8150f
jennhuynh02 <jennhuynh02@gmail.com>
2020-08-01 20:22:45 -0700

    Annotating common chars problem in javascript

--- leet_code/javascript/commonChars2.js
+++ leet_code/javascript/commonChars2.js
@@ -1,11 +1,16 @@
 function commonChars (A) {
   common = []; // O(1), space
   firstWord = A[0].split(""); // O(m), space
+  // Use first word to iterate through to check if all other words share common characters (choice of which word shouldn't matter)
 
   firstWord.forEach(char => { // O(m)
+    // Iterates through each character of the first word
     if (A.every(word => word.includes(char))) { // O(nm)
+      // Iterates through every word to check if every one of them includes that character of the first word
       A.map(word => word.replace(char, '')) // O(nm)
+      // Iterates through every word to replace a character in each word
       common.push(char);  // O(1), space
+      // Pushes this common character into common array to be return as common chars result
     }
   });
 
