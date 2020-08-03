763d3199025f996a3e87a1de85b617f3f2bfe842
jennhuynh02 <jennhuynh02@gmail.com>
2020-08-02 21:23:02 -0700

    Testing javascripts in place sort method

--- leet_code/javascript/countingElements2.js
+++ leet_code/javascript/countingElements2.js
@@ -37,11 +37,35 @@ function countingElements2(arr) {
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 
+function countingElements3(arr) {
+  count = 0; // O(1)
+  let streak = 1; // O(1)
+  arr.sort((a, b) => a - b); // O(n log n) - JS sort method sorts in place so no extra space required
+
+  for (let i = 0; i < arr.length; i++) { // O(n)
+    if (arr[i] === arr[i + 1]) { // O(1) 
+      streak += 1; // O(1)
+    } else if (arr[i] !== arr[i + 1] && arr[i + 1] === (arr[i] + 1)) { // O(1)
+      count += streak; // O(1)
+      streak = 1; // O(1)
+    } else {
+      streak = 1; // O(1)
+    };
+  };
+
+  return count; // O(1)
+};
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
+
 let arr = [1, 1, 3, 3, 5, 5, 7, 7];
-console.log(countingElements2(arr));
+console.log(countingElements3(arr));
 let arr1 = [1, 3, 2, 3, 5, 0];
-console.log(countingElements2(arr1));
+console.log(countingElements3(arr1));
 let arr2 = [1, 1, 2];
-console.log(countingElements2(arr2));
+console.log(countingElements3(arr2));
 let arr3 = [1, 1, 2, 2];
-console.log(countingElements2(arr3));
+console.log(countingElements3(arr3));
+let arr4 = [4, 10, 11, 11, 1, 9, 6, 2, 4, 5, 8];
+console.log(countingElements3(arr4));
