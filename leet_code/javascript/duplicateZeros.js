04b34de5e2ef4062b2a15ce48df479324289021c
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-09 14:27:52 -0700

    Solve duplicate zeros problem in js

--- leet_code/javascript/duplicateZeros.js
+++ leet_code/javascript/duplicateZeros.js
@@ -1,33 +1,24 @@
 function duplicateZeros(arr) {
-  let zeros = 0;
-  let regs = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if ((2*zeros + regs) < arr.length) {
-      if (arr[i] === 0) {
-        zeros += 1;
-      } else {
-        regs += 1;
-      }
+  for (let i = 0; i < arr.length; ++i) {  // O(n)
+    // travels through length of the array
+    if (arr[i] == 0) {  // O(1)
+      // if element at index i is 0
+      for (let j = arr.length - 1; j > i; j--) arr[j] = arr[j - 1]; // O(n)
+      // start iterating at the last index, copy the previous index's value over up until where the 0 is (hence, two zeros and array has shifted over)
+      i++;
+      // increment the loop
     }
   }
-
-  let next = arr.length - 1;
-  for (let j = arr.length - zeros - 1; j >= 0; j--) {
-    if (arr[j] !== 0) {
-      arr[next] = arr[j];
-      next -= 1;
-    } else {
-      arr[next] = arr[j];
-      arr[next - 1] = arr[j];
-      next -= 2;
-    }
-  }
-
   return arr;
-
 }
 
-let arr = [1, 0, 2, 3, 0, 4, 5, 0];
+
+// let arr = [1, 0, 2, 3, 0, 4, 5, 0];
+// let arr = [1,0,2,3,0,4,5,0];
+let arr = [8, 4, 5, 0, 0, 0, 0, 7];
 let arr1 = [1, 2, 3];
 
 console.log(duplicateZeros(arr));
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
