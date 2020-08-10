5b6d483c9fcaed90f7fbbc4c629e672704278313
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-10 03:26:29 -0700

    Analyze first unique char in js

--- leet_code/javascript/firstUnique.js
+++ leet_code/javascript/firstUnique.js
@@ -20,11 +20,30 @@ function firstUnique(s) {
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
+// preferred if space is not the concern
+
+function firstUnique2(s) {
+  for (i = 0; i < s.length; i++) {  // O(n)
+    // iterates through the length of the string
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {  // O(n + n)
+      // if the first index of that character is the same as the last index of that character
+      return i;  // O(1)
+      // return that index
+    }
+  }
+  return -1;  // O(1)
+}
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+// preferrd if space is the concern
 
 let s = "leetcode";
 let s1 = "loveleetcode";
 
 console.log(firstUnique(s))
 console.log(firstUnique(s1))
+console.log(firstUnique2(s))
+console.log(firstUnique2(s1))
 
 
