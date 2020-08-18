8845b3f27401d9bd955bfbcaf682d740a724afa7
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-18 15:00:57 -0700

    Solve reverse words in a string problem in javascript

--- leet_code/javascript/reverseWords.js
+++ leet_code/javascript/reverseWords.js
@@ -14,5 +14,10 @@ const reverse = word => {
   return reverseThis.join("");
 }
 
+const reverseWords2 = s => {
+  return s.split(" ").map(word => word.split("").reverse().join("")).join(" ");
+}
+
 let s = "Let's take LeetCode contest";
 console.log(reverseWords(s));
+console.log(reverseWords2(s));
