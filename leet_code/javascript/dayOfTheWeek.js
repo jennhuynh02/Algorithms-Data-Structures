e03709639d5a67824fd35e745e4ca06d017562d2
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-19 06:51:17 -0700

    Clean solution for day of the week problem in javascript

--- leet_code/javascript/dayOfTheWeek.js
+++ leet_code/javascript/dayOfTheWeek.js
@@ -1,14 +1,18 @@
-// const dayOfTheWeek = (day, month, year) => {
-//   const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];  // O(1), space
-//   return weekDay[new Date(year, month - 1, day).getDay()];  // O(1)
-// }
+// Solution 1:
+const dayOfTheWeek1 = (day, month, year) => {
+  const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];  // O(1), space
+  return weekDay[new Date(year, month - 1, day).getDay()];  // O(1)
+}
 
 /*
+Uses Date constructor - creates date and gets days of the week.
 Time Complexity:  O(1)
 Space Complexity:  O(1)
 */
 
-// const dayOfTheWeek2 = (day, month, year) => ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][new Date(year, month - 1, day).getDay()];
+
+// Solution 2:
+const dayOfTheWeek2 = (day, month, year) => ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][new Date(year, month - 1, day).getDay()];
 
 /*
 Same idea, just in one line.
@@ -19,7 +23,8 @@ Space Complexity:  O(1)
 // need to solve it without using the javascript Date class
 // JS months start at 0
 
-const dayOfTheWeek = (day, month, year) => {
+// Solution 3:
+const dayOfTheWeek3 = (day, month, year) => {
   let adjustedMonth = month - 1;
   const weekDays = [
     'Sunday',
@@ -68,8 +73,13 @@ const daysInYear = year => isLeapYear(year) ? 366 : 365
 // Wiki: "Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400."
 const isLeapYear = year => year % 4 === 0 && year % 100 !== 0 || year % 400 === 0
 
+// Solution 4:
+const dayOfTheWeek4 = day, month, year => 
+
 let day = 31;
 let month = 8;
 let year = 2019;
 
-console.log(dayOfTheWeek(day, month, year));
+console.log(dayOfTheWeek1(day, month, year));
+console.log(dayOfTheWeek2(day, month, year));
+console.log(dayOfTheWeek3(day, month, year));
