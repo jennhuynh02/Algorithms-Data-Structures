35c78bad0ff9ad63bb9d55d06196cc180f5c0a06
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-19 03:21:50 -0700

    Analyze reverse words in string problem in javascript

--- leet_code/javascript/reverseWords.js
+++ leet_code/javascript/reverseWords.js
@@ -1,22 +1,34 @@
 const reverseWords = s => {
   return s.split(" ").map(word => reverse(word)).join(" ");
+  // O(n) space + O(n^2) space + O(n) 
 }
 
 // Helper function to reverse words
 const reverse = word => {
-  let max = word.length - 1;
-  let mid = Math.ceil(max/2);
-  let reverseThis = word.split("");
+  let max = word.length - 1; // O(1)
+  let mid = Math.ceil(max / 2); // O(1)
+  let reverseThis = word.split(""); // O(n) space 
 
-  for (let i = 0; i < mid; i++) {
-    [reverseThis[i], reverseThis[max - i]] = [reverseThis[max - i], reverseThis[i]]
+  for (let i = 0; i < mid; i++) { // O(n)
+    [reverseThis[i], reverseThis[max - i]] = [reverseThis[max - i], reverseThis[i]] // O(1)
   }
-  return reverseThis.join("");
+  return reverseThis.join(""); // O(n)
 }
+
+/*
+Time Complexity:  O(n^2)
+Space Complexity:  O(n^2)
+*/
 
 const reverseWords2 = s => {
   return s.split(" ").map(word => word.split("").reverse().join("")).join(" ");
+  // O(n) space + O(n) * O(n space + n + n + n space) + O(n) space  
 }
+
+/*
+Time Complexity:  O(n^2)
+Space Complexity:  O(n^2)
+*/
 
 let s = "Let's take LeetCode contest";
 console.log(reverseWords(s));
