62520247aa187e29329dd4b9303c5adda5eee8b1
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-24 05:51:20 -0700

    Analyze benefit of constraint for rotate string problem

--- leet_code/javascript/rotateString.js
+++ leet_code/javascript/rotateString.js
@@ -1,19 +1,13 @@
 //  Problem 796
 const rotateString1 = (A, B) => {
-
   if (A === B) return true;  // O(1)
 
-  let check = A.slice(1) + A[0];  // O(1), O(n)space
-
   for (let i = 0; i < A.length; i++) {  // O(n)
-    if (check === B) {  // O(1)
-      return true;  // O(1)
-    } else {
-      check = check.slice(1) + check[0];  // O(1), O(n)space
-    }
+    A = A.slice(1) + A[0];  // O(1), O(n)space
+    if (A === B) return true;  // O(1)
   }
 
-  return false;
+  return false;  // O(1)
 }
 
 /*
@@ -34,8 +28,30 @@ const rotateString2 = (A, B) => {
 Solution 2:
 Time Complexity:  O(n)
 Space Complexity:  O(n)
+
+Concatenates A to itself, which should include all left shift patterns, and then checks if it includes B.
+*/
+
+const rotateString3 = (A, B) => {
+  if (A === B) return true;  // O(1)
+  if (A.length !== B.length) return false;  // O(1)
+
+  for (let i = 0; i < A.length; i++) {  // O(n)
+    A += A[0];  // O(1), space
+    A = A.replace(A[0], "");  // O(1), O(n) space
+    if (A === B) return true;  // O(1)
+  };
+  
+  return false;  // O(1)
+}
+
+/*
+Solution 3:
+Time Complexity:  O(n)
+Space Complexity:  O(n)
+Similar to solution 1, but with length constraint that could save time and memory if result is to be false due to difference in length.
 */
 
 let A = "abcde";
 let B = "cdeab";
-console.log(rotateString2(A,B));
+console.log(rotateString1(A,B));
