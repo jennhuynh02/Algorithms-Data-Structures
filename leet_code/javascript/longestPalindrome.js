0f5f425cdbe1c98a468bd54b3db0bf81dd048d76
Jennifer Huynh <jennhuynh02@gmail.com>
2020-09-05 06:44:47 -0700

    Leetcode longest palindrome

--- leet_code/javascript/longestPalindrome.js
+++ leet_code/javascript/longestPalindrome.js
@@ -21,9 +21,9 @@ const longestPalindrome = s => {
 Solution 1:
 
 Idea:
-1.  Initialize a set
-2.  Iterate through each char of the string, if it does not currently exist in the tracker, we will add it to the hash set, else, we will delete it from the tracker because it means that there was a duplicate of that char that could help assemble a palindrome
-3.  If the tracker has characters remaining (characters that do not have a duplicate counterpart), we will return the difference of the string's length and the size of the tracker; subtract -1 from tracker because palindromes may have one unique character in the middle
+1.  Initialize a const tracker to an empty hash set to keep track of what characters has a duplicate counterpart that could assist in forming the longest palindrome
+2.  Iterate through each char of the string, if it does not currently exist in the tracker, we will add it to the set, else, we will delete it from the tracker because it means that there was a duplicate of that char that could help assemble the longest palindrome
+3.  If the tracker has characters remaining (characters that do not have a duplicate counterpart to form the longest palindrome) - which would result in a truthy value, we will return the difference of the string's length and the size of the tracker - 1; subtract -1 from tracker because palindromes may have one unique character in the middle
 4.  If the tracker size is 0, it will be considered a falsey value; therefore, we would return the length of the string following the else statement in our if condition
 Time Complexity:  O(n)
 Space Complexity:  O(n) - worst case scenario if s contains all unique characters
