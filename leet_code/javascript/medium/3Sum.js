6be11865b1732fc47718925bfe26f17ac48597aa
Jennifer Huynh <jennhuynh02@gmail.com>
2020-10-07 00:17:28 -0700

    Solve leetcode problem 15 3sum

---
+++ leet_code/javascript/medium/3Sum.js
@@ -0,0 +1,47 @@
+// Problem 15:  3Sum
+
+// Approach 1:  Sort array, start at first index, add to the left and right of the remaining array, increment whichever side that would most likely result in a 0 sum
+
+const threeSum = nums => {
+  nums.sort((a, b) => a - b);
+
+  const results = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        results.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left] === nums[left + 1]) left++
+        left++;
+
+        while (right > left && nums[right] === nums[right - 1]) right--;
+        right--;
+
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    // First condition of this while loop is necessary because if somehow the end is a falsey value, and it compared with an undefined, whichi is also a falsey value, it could end up in an infinite loop
+    while (i < nums.length - 1 && nums[i] == nums[i + 1]) i++
+  }
+
+  return results;  
+};
+
+let nums = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums));
+
+/*
+Solution 1:
+Time Complexity: O(n^2) - While loop within for loop, which iterates through the remaining array for each iteration of the for loop
+Space Complexity: O(n) - Space for each element of the array, as in merge sort
+*/
