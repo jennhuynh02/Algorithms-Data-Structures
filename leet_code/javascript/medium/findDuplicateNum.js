f401d281a4ac0c72eba33202ddd82ec234f551fa
Jennifer Huynh <jennhuynh02@gmail.com>
2020-10-15 17:37:15 -0700

    Solve problem 287 duplicate num

---
+++ leet_code/javascript/medium/findDuplicateNum.js
@@ -0,0 +1,24 @@
+// Problem 287 Find the Duplicate Number
+
+// Approach 1 - Exploits array indexing property; sets integer at a specific index to be negative; if it is visited again (we check by seeing if there's a negative integer there) we return the duplicate number.
+
+const findDuplicateNum = nums => {
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = Math.abs(nums[i]);
+    if (nums[num-1] < 0) return num;
+    nums[num - 1] *= -1;
+  }
+
+};
+
+let nums = [1, 3, 4, 2, 2];
+let nums1 = [3, 1, 3, 4, 2];
+console.log(findDuplicateNum(nums));
+console.log(findDuplicateNum(nums1));
+
+/*
+Solution 1:
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
