836e2a4c0de2773fff9f4f8f3ba617e1fac94479
Jennifer Huynh <jennhuynh02@gmail.com>
2020-10-23 02:58:57 -0700

    Analyze problem 279 perfect squares

--- leet_code/javascript/medium/perfectSquare.js
+++ leet_code/javascript/medium/perfectSquare.js
@@ -1,6 +1,7 @@
 // Problem 279 Perfect Squares
 
-// Approach 1
+// Approach 1 - uses dynamic programming, each index accounts for how many primes are needed to sum to its numeric value
+
 
 const perfectSquares = n => {
   const tracker = Array(n + 1).fill(Infinity);
@@ -9,13 +10,14 @@ const perfectSquares = n => {
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j * j <= i; j++) {
       tracker[i] = Math.min(tracker[i], tracker[i - j*j] + 1)
+      // subtracts the largest prime number that fits within the number, and find its remainder's index, adds the number of primes that would make up that number plus 1, to account for the prime we just subtracted i by
     }
   }
 
-  return tracker;
+  return tracker[n];
 };
 
-let n = 9;
+let n = 12;
 console.log(perfectSquares(n));
 
 /*
