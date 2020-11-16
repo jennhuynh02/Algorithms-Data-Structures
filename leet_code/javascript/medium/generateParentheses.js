18375a74dbb14a7e2123cae320cf75e79f7c360a
Jennifer Huynh <jennhuynh02@gmail.com>
2020-11-16 01:34:48 -0800

    Analyze problem 22 generate parentheses

--- leet_code/javascript/medium/generateParentheses.js
+++ leet_code/javascript/medium/generateParentheses.js
@@ -5,6 +5,7 @@ const generateParentheses = n => {
   const result = [];
 
   const go = (str, open, close) => {
+    console.log(open, close, str)
     if (str.length === n * 2) result.push(str);
     if (open > 0) go(str + '(', open - 1, close + 1);
     if (close > 0) go(str + ')', open, close - 1);
@@ -17,10 +18,10 @@ const generateParentheses = n => {
 /*
 Solution 1:
 Time Complexity: O(4^n / n√n)
-Space Complexity: O(2^n)
+Space Complexity: O(4^n / n√n)
 */
 
 let n1 = 3;
-let n2 = 1;
+let n2 = 2;
 console.log(generateParentheses(n1));
 console.log(generateParentheses(n2));
