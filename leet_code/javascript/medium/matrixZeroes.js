6997d3e77f0cd6e6893840054b4b42ad5be651c6
Jennifer Huynh <jennhuynh02@gmail.com>
2020-11-19 15:27:45 -0800

    Solve problem 73 set matrix zeros in place

--- leet_code/javascript/medium/matrixZeroes.js
+++ leet_code/javascript/medium/matrixZeroes.js
@@ -38,7 +38,7 @@ Space Complexity: O(m + n) - accounts for col, row indices where 0's are found
 
 
 // Approach 2
-const setMatrixZeroes = matrix => {
+const setMatrixZeroes2 = matrix => {
   const height = matrix.length;
   const width = matrix[0].length;
   const col = new Set();
@@ -68,5 +68,56 @@ Time Complexity: O(m * n) - iterates through each element to check for 0's first
 Space Complexity: O(m) - set containing column indices that will need to be converted to 0's
 */
 
-let matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]];
+// Approach 3  Modify in place
+
+const setMatrixZeroes = matrix => {
+  const height = matrix.length;
+  const width = matrix[0].length;
+  let fillStartCol = false;
+  let fillStartRow = false;
+
+  for (let j = 0; j < width; j++) {
+    if (matrix[0][j] === 0) {
+      fillStartRow = true;
+    }
+  }
+
+  for (let j = 0; j < height; j++) {
+    if (matrix[j][0] === 0) {
+      fillStartCol = true;
+    }
+  }
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  for (let i = 1; i < height; i++) {
+    if (matrix[i][0] === 0) {
+      for (let j = 0; j < width; j++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  const start = fillStartCol ? 0 : 1;
+  for (let j = start; j < width; j++) {
+    if (matrix[0][j] === 0) {
+      for (let i = 0; i < height; i++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (fillStartRow) matrix[0].fill(0)
+
+  return matrix;
+}
+
+let matrix = [[-4, -2147483648, 6, -7, 0], [-8, 6, -8, -6, 0], [2147483647, 2, -9, -6, -10]]
 console.log(setMatrixZeroes(matrix));
