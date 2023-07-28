ae5df63afa7d6b188f353e93252d98c3cbbe6dc3
Jennifer Huynh <jennhuynh02@gmail.com>
2023-07-28 18:29:58 -0400

    Cleanup problem 2

--- leet_code/javascript/medium/addTwoNumbers.js
+++ leet_code/javascript/medium/addTwoNumbers.js
@@ -8,49 +8,6 @@ function ListNode(val, next) {
   this.next = (next === undefined ? null : next)
 }
 
-// Naive brute force approach
-const getNumber = (node) => {
-  const listOfNums = []
-
-  let currentNode = node
-  while (currentNode) {
-    listOfNums.push(currentNode.val)
-    if (currentNode.next) {
-      currentNode = currentNode.next
-    } else {
-      currentNode = null;
-    }
-  }
-
-  return BigInt((listOfNums.reverse().join('')).toString())
-}
-
-const createALinkedList = (list) => {
-  if (list.length < 1) return;
-
-  const head = new ListNode(list.pop());
-  let currNode = head;
-
-  while (list.length) {
-    const newNode = new ListNode(list.pop());
-    currNode.next = newNode;
-    currNode = newNode;
-  }
-
-  return head;
-}
-
-const addTwoNumbers = (l1, l2) => {
-  const firstNum = getNumber(l1)
-  const secondNum = getNumber(l2)
-  const total = firstNum + secondNum
-  console.log({ firstNum, secondNum, total })
-
-  const list = total.toString().split('') || []
-
-  return createALinkedList(list)
-}
-
 // Approach 1
 const addTwoNumbers1 = (l1, l2) => {
   const list = new ListNode(0)
@@ -87,5 +44,5 @@ const addTwoNumbers1 = (l1, l2) => {
 /*
 Solution 1:
 Time Complexity: O(max(m, n)) - will loop either m or n times, depending which is larger hence max(m,n)
-Space Complexity: O(max(m, n)) - reassigns the highest number variable in the for loop
+Space Complexity: O(max(m, n))
 */
